ef7fec3acf271a1cfcf4f42a4fc6e359f546f39a
Eric Saucedo <ericalexanders@gmail.com>
2024-05-17 01:43:40 -0300

    Ej 2 resuelto

--- logica/2.js
+++ logica/2.js
@@ -13,5 +13,21 @@
  */
 
 const t9Keyboard = value => {
-  return
+  const t9Mapping = {
+    '2': 'A', '22': 'B', '222': 'C',
+    '3': 'D', '33': 'E', '333': 'F',
+    '4': 'G', '44': 'H', '444': 'I',
+    '5': 'J', '55': 'K', '555': 'L',
+    '6': 'M', '66': 'N', '666': 'O',
+    '7': 'P', '77': 'Q', '777': 'R', '7777': 'S',
+    '8': 'T', '88': 'U', '888': 'V',
+    '9': 'W', '99': 'X', '999': 'Y', '9999': 'Z'
+  };
+  return value.split('-')
+    .map(pulsation => t9Mapping[pulsation] || '')
+    .join('')
 }
+
+// Prueba
+const salida = t9Keyboard('6-666-88-777-33-3-33-888');
+console.log(salida);
